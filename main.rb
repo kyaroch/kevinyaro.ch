1a7cbf6e0b7f2a222708e6af45935a66d8b9cc82
Kevin Yaroch <kjyaroch@gmail.com>
2014-07-08 00:14:40 -0700

    Working contact form (saves locally)

--- main.rb
+++ main.rb
@@ -1,5 +1,6 @@
 require "sinatra"
 require "sinatra/content_for"
+require "securerandom"
 
 helpers do
   def get_title(name)
@@ -10,6 +11,25 @@ helpers do
       "#{base_title} | #{name}"
     end
   end
+  
+  def record_message(info)
+    name, email, subject, message = info
+    begin
+      file_subj = subject.dup
+      if File.exist?("messages/#{subject}") || subject.empty?
+        file_subj << SecureRandom.hex
+      end
+      File.open("messages/#{file_subj}", "w") do |file|
+        file << "Name: #{name}\n"
+        file << "Email: #{email}\n"
+        file << "Time: #{Time.now.getutc}\n"
+        file << message
+      end
+      :success
+    rescue
+      :failure
+    end
+  end
 end
 
 get '/' do
@@ -18,6 +38,17 @@ end
 
 get '/links' do
   erb :links, :layout => :layout
+end
+
+get '/contact' do
+  erb :contact, :layout => :layout
+end
+
+post '/contact' do
+  info = params[:sender_name], params[:sender_email], params[:subject], params[:message]
+  @sent = record_message(info)
+  @name, @email, @subject, @message = info if @sent == :failure
+  erb :contact, :layout => :layout
 end
 
 get '/index' do
