0ddff59826982b388ee23c945fafe9156c6feacc
Kevin Yaroch <kjyaroch@gmail.com>
2014-07-08 21:08:32 -0700

    Added StatCounter

---
+++ public/statcounter.js
@@ -0,0 +1,8 @@
+var sc_project=9907617; 
+var sc_invisible=1; 
+var sc_security="aac3f1bf"; 
+var scJsHost = (("https:" == document.location.protocol) ?
+"https://secure." : "http://www.");
+document.write("<sc"+"ript type='text/javascript' src='" +
+scJsHost+
+"statcounter.com/counter/counter.js'></"+"script>");
